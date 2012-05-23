46c69a52b1e3dda518e44d17dfff420a3e407ea6
Sebastien COUDERT <coudert@lmlM6-82.univ-lille1.fr>
2012-05-23 16:48:29 +0200

    add direction based on sign of step modified:   stepper.h: add direction based on sign of step; comment serial write and read for debugging;

--- stepper.h
+++ stepper.h
@@ -35,23 +35,25 @@ public:
   bool move(const cimg_library::CImg<int> &step,const cimg_library::CImg<int> &velocity)
   {
     std::string command;
-    ///Set direction and velocity
-    command="+X;RX1000;";
+    ///Set direction and velocity (e.g. "+X;RX1000")
+    command=(step(0)<0)?std::string("-"):std::string("+");
+    command+=std::string("X;");//e.g. +X;
+    command+="RX1000;";//e.g. "RX1000"
 std::cerr<<"Set direction and velocity: write=|"<<command<<"|"<<std::flush;
-    comStepper.writes(command);
-    comStepper.reads(command);
+//    comStepper.writes(command);
+//    comStepper.reads(command);
 std::cerr<<"read=|"<<command<<"|\n"<<std::flush;
     ///Set displacement
     command="NX1;";
 std::cerr<<"Set displacement: write=|"<<command<<"|"<<std::flush;
-    comStepper.writes(command);
-    comStepper.reads(command);
+//    comStepper.writes(command);
+//    comStepper.reads(command);
 std::cerr<<"read=|"<<command<<"|\n"<<std::flush;
     ///Execute displacement
     command="MX;";
 std::cerr<<"Set displacement: write=|"<<command<<"|"<<std::flush;
-    comStepper.writes(command);
-    comStepper.reads(command);
+//    comStepper.writes(command);
+//    comStepper.reads(command);
 std::cerr<<"read=|"<<command<<"|\n"<<std::flush;
     return true;
   }//move
