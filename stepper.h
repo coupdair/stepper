244e3a633c8d00299543d10b55a6ca9f591082e4
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2012-05-29 22:53:07 +0200

    add set value at run time (Work In Progress). modified:   serial_stepper.help.output: automatic update. modified:   stepper.h: add both set_step_command function and axis data in Cstepper class.

--- stepper.h
+++ stepper.h
@@ -1,6 +1,7 @@
 #ifndef STEPPER
 #define STEPPER
 
+#include <vector>
 #include "../rs232/serial_factory.h"
 
 class Cstepper
@@ -16,16 +17,23 @@ private:
   //! communication with stepper
   Cserial* pComStepper;
 public:
+  std::vector<std::string> axis;
   //! constructor
   /**
    *
   **/
   Cstepper()
   {
+    ///set class name (debug only)
 #if cimg_debug>1
-    class_name="Cstepper";
+    class_name="Cstepper_uControlXYZ";
 #endif
+    ///set library version (information only)
     class_version=VERSION;
+    ///set axis names
+    axis.push_back("X");
+    axis.push_back("Y");
+    axis.push_back("Z");
   }//constructor
 
   //! Open stepper device
@@ -45,7 +53,24 @@ public:
 
     return pComStepper->opens(port_path);
   }//open
-  
+
+  //! set step command
+  /** 
+   *
+   * @param[in] step=number of step to do
+   * @param[in] axis=translation along axis (as string; e.g. "X")
+   *
+   * @return message (i.e. string to send to serial port)
+   */
+  const std::string set_step_command(int step,const std::string axis)
+  {
+    const std::string prefix="N";
+    const std::string formated_step="123";//! \todo int to string
+    const std::string suffix=";";
+    const std::string command=prefix+axis+formated_step+suffix;
+    return command;//e.g. "NX123;"
+  }
+
   //! move once using 3D step (and 3D velocity)
   /** 
    *
@@ -65,7 +90,7 @@ std::cerr<<"Set direction and velocity: write=|"<<command<<"|"<<std::flush;
 //    pComStepper->reads(command);
 //std::cerr<<"read=|"<<command<<"|\n"<<std::flush;
     ///Set displacement
-    command="NX1;";
+    command=set_step_command(step[0],axis[0]);//e.g. "NX123;"
 std::cerr<<"Set displacement: write=|"<<command<<"|"<<std::flush;
     pComStepper->writes(command);
 //    pComStepper->reads(command);
