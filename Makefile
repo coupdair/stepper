8f2ac0843132eb44471b6bd4f530ff92a555cb17
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2013-01-28 16:32:03 +0100

    NewPortXYZ axis stepper (i.e. ESP300 with 3 axes) modified:   Makefile: upgrade version for dev to v0.2.2 . modified:   VERSION: v0.2.2.dev . modified:   stepper.h: add Cstepper_NewPortXYZ class from Cstepper_uControlXYZ (actually a copy) modified:   stepper.help.output: auto updated.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 PROGRAMS = version stepper
 DOCUMENTATIONS = doc
 
-version = v0.2.1
+version = v0.2.2
 #version_cimg = 125
 version_cimg = 149
 version_rs232=`cat ../rs232/VERSION`
--- stepper.h
+++ stepper.h
@@ -611,5 +611,174 @@ cimg_library::cimg::wait(2000);
 
 };//Cstepper_AVRmatrix class
 
+class Cstepper_NewPortXYZ: public Cstepper
+{
+protected:
+  //! communication with stepper
+  Cserial* pComStepper;
+public:
+  //! constructor
+  /**
+   *
+  **/
+  Cstepper_NewPortXYZ()
+  {
+    ///set class name (debug only)
+#if cimg_debug>1
+    class_name="Cstepper_NewPortXYZ";
+#endif
+    ///set library version (information only)
+    class_version=STEPPER_VERSION;
+    ///set axis names
+    axis_name.push_back("1");//X
+    axis_name.push_back("2");//Y
+    axis_name.push_back("3");//Z
+  }//constructor
+
+  //! Open stepper device
+  /** 
+   *
+   * @param[in] port_path: path of serial port device
+   * @param[in] port_type: type of serial port (see \c Cserial_factory:: )
+   *
+   * @return 
+   */
+  bool open(const std::string& stepper_port_path,const std::string& stepper_port_type,
+            const std::string&  reader_port_path,const std::string&  reader_port_type, const cimg_library::CImg<int> &jitter)
+  {
+#if cimg_debug>1
+std::cerr<<class_name<<"::"<<__func__<<"("<<stepper_port_path<<","<<stepper_port_type<<")\n"<<std::flush;
+#endif
+    //choose serial
+    Cserial_factory serial_factory;
+    pComStepper=serial_factory.create(stepper_port_type);
+    //initialise
+
+    return pComStepper->opens(stepper_port_path);
+  }//open
+
+  //! \todo remove valueToString function should be in parent class !
+  //! convert value to string
+  /**
+   * format value to string
+  **/
+  template<typename T> std::string valueToString(const T& value)
+  {
+    std::ostringstream stream;
+    stream << value;
+    return stream.str();
+  }//valueToString
+
+  //! set both direction AND velocity command
+  /** 
+   *
+   * @param[in] step=number of step to do
+   * @param[in] velocity=velocity to go
+   * @param[in] axis=translation along axis (as string; e.g. "X")
+   *
+   * @return message (i.e. string to send to serial port)
+   */
+  const std::string set_directionNvelocity_command(const int step,const int velocity,const std::string axis)
+  {
+    const std::string direction=(step<0)?std::string("-"):std::string("+");
+    //axis
+    const std::string suffix=";";
+    const std::string prefix="R";
+    const std::string formated_velocity=valueToString(velocity);
+    const std::string command=direction+axis+suffix+prefix+axis+formated_velocity+suffix;
+    return command;//e.g. "+X;RX1000"
+  }
+
+  //! set step command
+  /** 
+   *
+   * @param[in] step=number of step to do
+   * @param[in] axis=translation along axis (as string; e.g. "X")
+   *
+   * @return message (i.e. string to send to serial port)
+   */
+  const std::string set_step_command(int step,const std::string axis)
+  {
+    const std::string prefix="N";
+    //axis
+    const std::string formated_step=valueToString(std::abs(step));
+    const std::string suffix=";";
+    const std::string command=prefix+axis+formated_step+suffix;
+    return command;//e.g. "NX123;"
+  }
+
+  //! move once using 1D step and 1D velocity along specific axis
+  /** 
+   *
+   * @param[in] axis_index=axis index (using axis[axis_index])
+   * @param[in] step=steps along specified axis
+   * @param[in] velocity=velocity for axis
+   *
+   * @return true on success, false otherwise
+   */
+  bool move(const int axis_index,const int step,const int velocity)
+  {
+    //null displacement
+    if(step==0) return true;
+    //other displacement
+    std::string command;
+    ///Set direction and velocity (e.g. "+X;RX1000;")
+    command=set_directionNvelocity_command(step,velocity,axis_name[axis_index]);
+std::cerr<<"Set direction and velocity: write=|"<<command<<"|"<<std::flush;
+    if(!pComStepper->writes(command)) return false;
+//    pComStepper->reads(command);
+//std::cerr<<"read=|"<<command<<"|\n"<<std::flush;
+    ///Set displacement
+    command=set_step_command(step,axis_name[axis_index]);//e.g. "NX123;"
+std::cerr<<"Set displacement: write=|"<<command<<"|"<<std::flush;
+    if(!pComStepper->writes(command)) return false;
+//    pComStepper->reads(command);
+//std::cerr<<"read=|"<<command<<"|\n"<<std::flush;
+    ///Execute displacement
+    command="M"+axis_name[axis_index]+";";//e.g. "MX;";
+std::cerr<<"Make displacement: write=|"<<command<<"|"<<std::flush;
+    if(!pComStepper->writes(command)) return false;
+//    pComStepper->reads(command);
+//std::cerr<<"read=|"<<command<<"|\n"<<std::flush;
+    return true;
+  }//move
+
+  //! move once using 3D step (and 3D velocity)
+  /** 
+   *
+   * @param[in] step=steps in the 3 directions (XYZ)
+   * @param[in] velocity=velocities in the 3 directions (XYZ)
+   *
+   * @return true on success, false otherwise
+   */
+  bool move(const cimg_library::CImg<int> &step,const cimg_library::CImg<int> &velocity)
+  {
+    cimg_forX(step,i)
+    {
+      if(!move(i,step(i),velocity(i)))
+      {
+        std::cerr<<"error: while moving "<<axis_name[i]<<" axis (i.e. index="<<i<<").\n"<<std::flush;
+        return false;
+      }
+      //wait for the movement ends
+//! \todo use blocking RS232 functions
+      if(step(i)!=0) cimg_library::cimg::wait(1000);
+    }//axis loop
+    return true;
+  }//move
+
+  //! Close stepper
+  /** 
+   *
+   * @return 
+   */
+  bool close()
+  {
+    return pComStepper->closes();
+  }//close
+
+};//Cstepper_NewPortXYZ class
+
+
 #endif //STEPPER
 
